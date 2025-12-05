7a69821597e12acf136043f00a29ccdf50ff68fe
zinkosuke <48650930+zinkosuke@users.noreply.github.com>
2025-12-05 11:16:50 +0900

    fix: 米澤さんの修正一部再現(まだ動かない)

--- src/events/http/HttpServer.js
+++ src/events/http/HttpServer.js
@@ -430,6 +430,10 @@ export default class HttpServer {
       stage,
     } = params
 
+    console.error("**************************************************")
+    console.error(params)
+    console.error("**************************************************")
+
     return async (request, h) => {
       const requestPath =
         endpoint.isHttpApi || this.#options.noPrependStageInUrl
@@ -611,11 +615,20 @@ export default class HttpServer {
 
       let result
       let err
+      let resultStatus = ''
 
       try {
         result = await lambdaFunction.runHandler()
       } catch (_err) {
         err = _err
+      }
+      // カスタム例外をraiseさせないように変更している
+      // see src/lambda/handler-runner/python-runner/invoke.py
+      try {
+        resultStatus = result.status
+        result = result.value
+      } catch (e) {
+        console.error(e)
       }
 
       // const processResponse = (err, data) => {
@@ -630,9 +643,37 @@ export default class HttpServer {
 
       // Failure handling
       let errorStatusCode = "502"
+      let errorList = ''
+      let errorMessage = ''
 
-      if (err) {
-        const errorMessage = (err.message || err).toString()
+      if (resultStatus === 'fail') {
+        if (endpoint.response && endpoint.response.statusCodes) {
+          const { statusCodes } = endpoint.response
+          Object.keys(statusCodes).some((key) => {
+            const { pattern } = statusCodes[key]
+            const regex = new RegExp(`^${pattern}$`)
+            if (regex.test(result)) {
+              console.log('key', key)
+              console.log('pattern', pattern)
+              try {
+                console.log('result2', result)
+                errorList = JSON.parse(result)
+              } catch (e) {
+                errorList = { type: result }
+                console.error(e)
+              }
+              err = result
+              errorStatusCode = key
+              return true
+            }
+            return false
+          })
+        }
+      }
+      console.log('errorList', errorList)
+
+      if (err && errorStatusCode === '502') {
+        errorMessage = (err.message || err).toString()
 
         const found = errorMessage.match(/\[(\d{3})]/)
 
@@ -660,6 +701,19 @@ export default class HttpServer {
             break
           }
         }
+      } else if (err) {
+        errorMessage = err.toString()
+        // Mocks Lambda errors
+        result = {
+          errorMessage,
+          errorType: err.toString(),
+          stackTrace: err.toString(),
+        }
+        responseName = errorStatusCode
+        Object.keys(errorList).forEach((key) => {
+          console.log('key2', key)
+          result[key] = errorList[key]
+        })
       }
 
       log.debug(`Using response '${responseName}'`)
@@ -698,6 +752,10 @@ export default class HttpServer {
                 headerValue = valueArray[3]
                   ? jsonPath(result, valueArray.slice(3).join("."))
                   : result
+
+                if(valueArray.length == 5 && valueArray[3] == "errorMessage"){
+                  headerValue = valueArray[4] ? jsonPath(result, valueArray.slice(4).join('.')) : result;
+                }  
 
                 headerValue = headerValue == null ? "" : String(headerValue)
               } else {
--- src/events/http/OfflineEndpoint.js
+++ src/events/http/OfflineEndpoint.js
@@ -30,4 +30,34 @@ export default class OfflineEndpoint {
   }
 
   type = "AWS"
+
+  constructor(statusCodes = null) {
+    if (statusCodes === null) {
+      return
+    }
+    Object.keys(statusCodes).forEach((key) => {
+      const statusCode = statusCodes[key];
+      const responseParameters = {};
+      if ("headers" in statusCode){
+        Object.keys(statusCode["headers"]).forEach((headerKey) => {
+            responseParameters["method.response.header."+headerKey] = statusCode["headers"][headerKey];
+        });
+      }
+      this.responses[key] = {
+        responseModels: {
+          'application/json;charset=UTF-8': 'Empty',
+        },
+        responseParameters: responseParameters,
+        responseTemplates: {
+          'application/json;charset=UTF-8': '',
+        },
+        statusCode: key,
+      }
+      this.responses[key][key] = {
+        statusCode: key,
+      }
+    })
+    // console.error("===== Custom offline endpoint =====")
+    // console.error(this.responses)
+  }
 }
